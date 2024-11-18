356b7485bb7ab21065aaca4f124f3f3f17fdb48e
Ruslan Guskov <feniksuh@gmail.com>
2024-11-18 16:56:16 +0300

    fix: import Media class

--- config/chat.php
+++ config/chat.php
@@ -1,12 +1,13 @@
 <?php
 
 use App\Models\User;
+use RonasIT\Media\Models\Media;
 use Illuminate\Notifications\Channels\BroadcastChannel;
 
 return [
     'classes' => [
         'user_model' => User::class,
-        'media_model' => RonasIT\Media\Models\Media::class,
+        'media_model' => Media::class,
     ],
     'default_channels' => [
         BroadcastChannel::class
