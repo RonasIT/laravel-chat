d6e63861b137fd9df451e1c456007197bdace948
DenTray <dpankratov@ronasit.com>
2026-03-04 17:56:40 +0600

    Apply suggestion from @DenTray

--- src/Notifications/ConversationDeletedNotification.php
+++ src/Notifications/ConversationDeletedNotification.php
@@ -6,7 +6,6 @@ use Illuminate\Broadcasting\PrivateChannel;
 use Illuminate\Bus\Queueable;
 use Illuminate\Contracts\Broadcasting\ShouldBroadcast;
 use Illuminate\Contracts\Queue\ShouldQueue;
-use Illuminate\Database\Eloquent\Model;
 use Illuminate\Notifications\Messages\BroadcastMessage;
 use Illuminate\Notifications\Notification;
 use RonasIT\Chat\Contracts\Notifications\ConversationDeletedNotificationContract;
