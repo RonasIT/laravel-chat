0a7cdc0188bcf7f4953d1b9a6e63d480eeb62dc5
vgrams <vgrams@ronasit.com>
2026-04-17 12:52:38 +0600

    fix: remove redundant code since it's already implemented in the laravel-helpers package and causes a type-hint error
    refs: https://github.com/RonasIT/laravel-chat/issues/61

--- src/Repositories/BaseRepository.php
+++ src/Repositories/BaseRepository.php
@@ -2,32 +2,8 @@
 
 namespace RonasIT\Chat\Repositories;
 
-use Illuminate\Support\Arr;
 use RonasIT\Support\Repositories\BaseRepository as RonasITRepository;
 
 class BaseRepository extends RonasITRepository
 {
-    public function insertOrIgnore(array $data): bool
-    {
-        $defaultTimestamps = [];
-
-        if ($this->model->timestamps) {
-            $now = now();
-
-            $defaultTimestamps = [
-                $this->model::CREATED_AT => $now,
-                $this->model::UPDATED_AT => $now,
-            ];
-        }
-
-        $data = array_map(function ($item) use ($defaultTimestamps) {
-            $fillableFields = Arr::only($item, $this->model->getFillable());
-
-            return array_merge($defaultTimestamps, $fillableFields);
-        }, $data);
-
-        $this->postQueryHook();
-
-        return $this->model->insertOrIgnore($data);
-    }
 }
